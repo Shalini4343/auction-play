1cc2bd1484ebebf64a06ed5c5a62494170406e9c
Shalini4343 <121305953+Shalini4343@users.noreply.github.com>
2024-03-10 22:05:33 +0530

    local to repo
    I have created a proj in the local and now shifted it to git for source tracking

---
+++ code.js
@@ -0,0 +1,14 @@
+console.log("Hello World");
+let firstName = "Shalini";
+let lastName = "R"
+console.log(firstName)
+function call(name) {
+    console.log("You are a genius "+name);
+}
+call("Shalini");
+
+function sqrt(num) {
+    return num*num;
+}
+let ans = sqrt(2);
+console.log(ans)
---
+++ index.js
@@ -0,0 +1,8 @@
+
+function max(num1,num2) {
+    return (num1>num2) ?  num1:num2;
+}
+let num1 = 5000;
+let num2 =1000;
+
+console.log(max(num1,num2));
